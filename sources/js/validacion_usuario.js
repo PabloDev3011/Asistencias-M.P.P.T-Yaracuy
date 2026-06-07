ab8b153e23cd0aabeccb4ed8f998c688cbe568d2
PabloDev3011 <pabloilarraza3011@gmail.com>
2026-06-06 21:36:05 -0400

    actualizacion añadido maestro de gestion de personal, validacion de index mejorado, añdaidio usuario y lcave al sistema, correcion de errores

---
+++ sources/js/validacion_usuario.js
@@ -0,0 +1,18 @@
+let user = "admin";
+let password = "1234";
+
+function validar_usr(obj) {
+  let usr = obj.usuario.value;
+  let pass = obj.clave.value;
+
+  if (usr != user) {
+    alert("Usuario incorrecto");
+    obj.usuario.focus;
+    return false;
+  }
+  if (pass != password) {
+    alert("Clave incorrecta");
+    obj.clave.focus;
+    return false;
+  }
+}
